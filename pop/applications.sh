be5615598c1475436f8dc145af18d513857bbe36
tvaidotas <tadas.vaidotas@gmail.com>
2020-10-18 20:57:01 +0100

    updating application list

--- pop/applications.sh
+++ pop/applications.sh
@@ -12,8 +12,11 @@ sudo apt install -y obs-studio
 sudo apt install -y default-jdk && sudo apt install -y default-jre
 sudo apt install maven -y
 sudo apt install python -y
-sudo apt install teamviewer -y
-sudo apt install sbt -y
+sudo apt install teamviewer -y # not found
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt-get update
+sudo apt-get install sbt
 sudo apt install curl -y
 sudo snap refresh
 sudo snap install postman
